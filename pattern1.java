cef212de12d29d08c07850490e7e58781a9f0812
KiranAkkal <64002805+KiranAkkal@users.noreply.github.com>
2020-04-23 11:25:30 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,19 @@
+class A
+{
+  public static void main(String[] args) 
+  {
+   int rows = 3;
+   int columns =3;
+   int k = 1;
+   for (int i =1; i <= rows;i++ ) 
+   {
+      for (int j = 1;j <= columns ;j++ ) 
+      {
+        System.out.print(k);
+        k++;
+
+      }
+      System.out.println();
+    } 
+  }
+}
