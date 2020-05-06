70406d902e473143797ffc4ee0efc645c6b7d976
KiranAkkal <64002805+KiranAkkal@users.noreply.github.com>
2020-05-06 12:20:06 +0530

    Add files via upload

---
+++ string1.java
@@ -0,0 +1,15 @@
+class A
+{
+	public static void main(String[] args) {
+		 chararray(); 
+		 
+		
+	}
+	public static void chararray() {
+		char[] cArray = {'B','O','Y'};
+		 System.out.print("Character retrived from cArray at index 1 = ");
+		 char v1 = cArray[2];
+		 System.out.println(v1);
+		
+	}
+}
